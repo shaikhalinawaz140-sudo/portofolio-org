5560da1723f8e695562f1453e6d2146a25309d87
shaikhalinawaz140-sudo <shaikhalinawaz140@gmail.com>
2026-02-25 04:35:40 +0500

    update changes

--- app.js
+++ app.js
@@ -24,4 +24,37 @@ let navlinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", function () {
   navlinks.classList.toggle("active");
+ });
+// form input 
+document.addEventListener("DOMContentLoaded", function() {
+// for name
+    const form = document.getElementById("cotactForm");
+    const nameInput = document.getElementById("name");
+    const nameError = document.getElementById("nameError");
+    const successMsg = document.getElementById("successMsg");
+
+// for email
+    const emailInput = document.getElementById("email");
+    const emailError = document.getElementById("emailError");
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        let isValid = true;
+
+        if(nameInput.value.trim() === ""){
+            nameError.textContent = "Name is required";
+            isValid = false;
+        } else {
+            nameError.textContent = "";
+        }
+
+        if(isValid){
+            successMsg.textContent = "Form submitted successfully !";
+            form.reset();
+        } else {
+            successMsg.textContent = "";
+        }
+    });
+
 });
