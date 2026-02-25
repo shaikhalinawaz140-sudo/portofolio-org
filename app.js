55ad1a0ffa1b7b6509b580b89b4e10c6d8a3402f
shaikhalinawaz140-sudo <shaikhalinawaz140@gmail.com>
2026-02-25 20:47:26 +0500

    update changes

--- app.js
+++ app.js
@@ -29,32 +29,60 @@ hamburger.addEventListener("click", function () {
 document.addEventListener("DOMContentLoaded", function() {
 // for name
     const form = document.getElementById("cotactForm");
+    
     const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("messageInput");
+
     const nameError = document.getElementById("nameError");
+    const emailError = document.getElementById("emailError");
+    const messageError = document.getElementById("messageError");
+
     const successMsg = document.getElementById("successMsg");
 
-// for email
-    const emailInput = document.getElementById("email");
-    const emailError = document.getElementById("emailError");
+// step 2 fom submit event
+form.addEventListener("submit", function(e){
+    e.preventDefault(); //page load na ho
 
-    form.addEventListener("submit", function(e){
-        e.preventDefault();
+    let isValid = true; //from k varible chike karo
+    nameError.textContent ="";
+    emailError.textContent="";
+    messageError.textContent="";
+    successMsg.textContent="";
 
-        let isValid = true;
+// step 3 validaton
+if(nameInput.value.trim() ===""){
+    nameError.textContent = "Name is required",
+    isValid = false;
+}    
+})
 
-        if(nameInput.value.trim() === ""){
-            nameError.textContent = "Name is required";
+//step 4 Email validation
+
+const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if(emailInput.value.trim()===""){
+    emailError.textContent ="Email is requird";
+    isValid = false;
+}
+else if(!emailPattern.test(emailInput.value.trim())){
+    emailError.textContent = "enter valid email";
+    isValid = false;
+}
+
+//step 5 message validation
+ if(messageInput.value.trim() === ""){
+            messageError.textContent = "Message is required";
             isValid = false;
-        } else {
-            nameError.textContent = "";
         }
-
-        if(isValid){
-            successMsg.textContent = "Form submitted successfully !";
-            form.reset();
-        } else {
-            successMsg.textContent = "";
-        }
-    });
-
+if(messageInput.value.trim(). lenght <10){
+    messageError.textContent = "Message must be at least 10 characters";
+    isValid=false;
+}
+//step 6 final check
+if (isValid){
+    successMsg.textContent = "Form submitted successfully"
+    successMsg.classListadd("success");
+    form.reset();
+}
 });
